2b4087a77dfa307b02ac02aeca5133ffdce40d2a
cloudliner <cloudliner.jp@gmail.com>
2018-01-21 17:11:24 +0900

    connected position

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, ViewContainerRef, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewContainerRef, ViewChild, ElementRef } from '@angular/core';
 import { Overlay, OverlayConfig } from '@angular/cdk/overlay';
 import { ComponentPortal } from '@angular/cdk/portal';
 import { CdkPortal } from '@angular/cdk/portal';
+import { MyFirstPanelComponent } from './my-first-panel/my-first-panel.component';
 
 @Component({
   selector: 'app-root',
@@ -13,15 +14,35 @@ export class AppComponent implements OnInit {
 
   @ViewChild(CdkPortal) templatePortal: CdkPortal;
 
-  constructor(private overlay: Overlay) { }
+  constructor(private overlay: Overlay, private elementRef: ElementRef) { }
 
   ngOnInit() {
-    const positionStrategy =
-      this.overlay.position().global().centerHorizontally().centerVertically();
-    const config = new OverlayConfig({
-      positionStrategy
-    });
-    const overlayRef = this.overlay.create(config);
-    this.templatePortal.attach(overlayRef);
+    {
+      const positionStrategy =
+        this.overlay.position().global().centerHorizontally().centerVertically();
+      const config = new OverlayConfig({
+        positionStrategy
+      });
+      const overlayRef = this.overlay.create(config);
+      this.templatePortal.attach(overlayRef);
+    }
+    {
+      const targetElementRef = 
+        new ElementRef(this.elementRef.nativeElement.querySelectorAll('.targetElement')[0]);
+      console.log(targetElementRef);
+      const positionStrategy = 
+        this.overlay.position().connectedTo(targetElementRef, {
+          originX: 'start',
+          originY: 'bottom'
+        }, {
+          overlayX: 'start',
+          overlayY: 'bottom'
+        }).withOffsetY(20);
+        const config = new OverlayConfig({
+          positionStrategy
+        });
+      const overlayRef = this.overlay.create(config);
+      overlayRef.attach(new ComponentPortal(MyFirstPanelComponent));
+    }
   }
 }
