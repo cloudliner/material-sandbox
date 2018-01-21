629e0f4cf0d5da954ea7dba7ea70eff2deedc603
cloudliner <cloudliner.jp@gmail.com>
2018-01-21 19:12:27 +0900

    simplify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Overlay, OverlayConfig } from '@angular/cdk/overlay';
 import { ComponentPortal } from '@angular/cdk/portal';
 import { CdkPortal } from '@angular/cdk/portal';
 import { MyFirstPanelComponent } from './my-first-panel/my-first-panel.component';
+import { GlobalPositionStrategy } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-root',
@@ -12,44 +13,18 @@ import { MyFirstPanelComponent } from './my-first-panel/my-first-panel.component
 export class AppComponent implements OnInit {
   title = 'app';
 
-  @ViewChild(CdkPortal) templatePortal: CdkPortal;
-
   constructor(private overlay: Overlay, private elementRef: ElementRef) { }
 
   ngOnInit() {
-    /*
-    {
-      const positionStrategy =
-        this.overlay.position().global().centerHorizontally().centerVertically();
-      const config = new OverlayConfig({
-        positionStrategy,
-        hasBackdrop: true
-      });
-      const overlayRef = this.overlay.create(config);
-      this.templatePortal.attach(overlayRef);
-    }
-    */
-    {
-      const targetElementRef = 
-        new ElementRef(this.elementRef.nativeElement.querySelectorAll('.targetElement')[0]);
-      console.log(targetElementRef);
-      const positionStrategy = 
-        this.overlay.position().connectedTo(targetElementRef, {
-          originX: 'start',
-          originY: 'bottom'
-        }, {
-          overlayX: 'start',
-          overlayY: 'bottom'
-        }).withOffsetY(20);
-        const config = new OverlayConfig({
-          positionStrategy
-        });
-      const overlayRef = this.overlay.create(config);
-      const componentPortal = new ComponentPortal<MyFirstPanelComponent>(MyFirstPanelComponent);
-      let conponent = componentPortal.component;
-      conponent['overlayRef'] = overlayRef;
-      conponent['componentPortal'] = componentPortal;
-      overlayRef.attach(componentPortal);
-    }
+    const positionStrategy =
+      this.overlay.position().global().centerHorizontally().centerVertically();
+    const config = new OverlayConfig({
+      positionStrategy
+    });
+    const overlayRef = this.overlay.create(config);
+    let position = overlayRef.getConfig().positionStrategy as GlobalPositionStrategy;
+    position.top('10');
+    const componentPortal = new ComponentPortal<MyFirstPanelComponent>(MyFirstPanelComponent);
+    overlayRef.attach(componentPortal);
   }
 }
