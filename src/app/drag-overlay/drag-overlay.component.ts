559703a5ed8fe854aa83ab6114dd07dc24dc27af
cloudliner <cloudliner.jp@gmail.com>
2018-01-21 20:07:23 +0900

    add global drag

--- src/app/drag-overlay/drag-overlay.component.ts
+++ src/app/drag-overlay/drag-overlay.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, ViewContainerRef, ViewChild, ElementRef } from '@angular/core';
-import { Overlay, OverlayConfig } from '@angular/cdk/overlay';
-import { ComponentPortal } from '@angular/cdk/portal';
+import { Overlay, OverlayRef, OverlayConfig, GlobalPositionStrategy } from '@angular/cdk/overlay';
 import { CdkPortal } from '@angular/cdk/portal';
 
 @Component({
@@ -9,18 +8,53 @@ import { CdkPortal } from '@angular/cdk/portal';
   styleUrls: ['./drag-overlay.component.scss']
 })
 export class DragOverlayComponent implements OnInit {
+  private positionStrategy: GlobalPositionStrategy;
+  private overlayRef: OverlayRef;
+  private startX :number;
+  private startY :number;
 
   @ViewChild(CdkPortal) templatePortal: CdkPortal;
 
   constructor(private overlay: Overlay, private elementRef: ElementRef) { }
 
   ngOnInit() {
-    const positionStrategy =
-      this.overlay.position().global().centerHorizontally().centerVertically();
+    this.positionStrategy =
+      this.overlay.position().global().left('100px').top('100px');
     const config = new OverlayConfig({
-      positionStrategy,
+      positionStrategy: this.positionStrategy
     });
-    const overlayRef = this.overlay.create(config);
-    this.templatePortal.attach(overlayRef);
+    this.overlayRef = this.overlay.create(config);
+    this.templatePortal.attach(this.overlayRef);
+  }
+
+  dragenter($event) {
+    console.log('dragenter:', $event);
+  }
+
+  dragover($event) {
+    console.log('dragover:', $event);
+  }
+
+  dragleave($event) {
+    console.log('dragleave:', $event);
+  }
+
+  dragend($event) {
+    console.log('dragend:', $event);
+    let x = $event.pageX;
+    let y = $event.pageY;
+    console.log('x:', x);
+    console.log('y:', y);
+    this.positionStrategy.left(x + 'px');
+    this.positionStrategy.top(y + 'px');
+    this.overlayRef.updatePosition();
+  }
+
+  drop($event) {
+    console.log('drop:', $event);
+  }
+
+  click($event) {
+    console.log('click:', $event);
   }
 }
