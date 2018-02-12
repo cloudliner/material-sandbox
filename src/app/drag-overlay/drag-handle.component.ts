e1dfa2be4deaefd96068c9d96385c8a6fb67ad63
cloudliner <cloudliner.jp@gmail.com>
2018-02-12 18:47:18 +0900

    fix

--- src/app/drag-overlay/drag-handle.component.ts
+++ src/app/drag-overlay/drag-handle.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, AfterViewInit, OnDestroy, ElementRef, ComponentRef } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy, ElementRef, ComponentRef, Inject, HostListener } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { Overlay, OverlayRef, OverlayConfig, GlobalPositionStrategy, OverlayContainer } from '@angular/cdk/overlay';
 import { ComponentPortal } from '@angular/cdk/portal';
 import { Observable } from 'rxjs/Observable';
@@ -28,7 +29,9 @@ export class DragHandleComponent implements OnInit, AfterViewInit, OnDestroy {
     resize: null
   };
 
-  constructor(private overlay: Overlay, private overlayContainer: OverlayContainer) { }
+  constructor(private overlay: Overlay,
+              private overlayContainer: OverlayContainer,
+              @Inject(DOCUMENT) private document: any) { }
 
   ngOnInit() {
     this.positionStrategy = this.overlay.position().global();
@@ -43,7 +46,7 @@ export class DragHandleComponent implements OnInit, AfterViewInit, OnDestroy {
     // Drag object
     const overlayPane = this.overlayRef.overlayElement;
     // Drop target area
-    const targetElementRef = new ElementRef(document.body);
+    const targetElementRef = new ElementRef(this.document.body);
 
     const dragstart$ = Observable.fromEvent(overlayPane, 'dragstart');
     const dragend$ = Observable.fromEvent(overlayPane, 'dragend');
@@ -52,22 +55,10 @@ export class DragHandleComponent implements OnInit, AfterViewInit, OnDestroy {
 
     this.subscriptions.drag = dragstart$.subscribe((event: DragEvent) => {
       console.log('dragstart:', event);
-      const instance: DraggableCompoent = componentRef.instance;
-      this.setDragImage(event, instance);
-      this.startX = event.pageX;
-      this.startY = event.pageY;
-
-      setTimeout(function() {
-        overlayPane.style.opacity = '0';
-      }, 0);
-
-      this.subscriptions.dragover = dragover$.subscribe((dragoverEvent: DragEvent) => {
-        dragoverEvent.preventDefault();
-      });
 
       this.subscriptions.drop = drop$.take(1).subscribe((dragEvent: DragEvent) => {
-        console.log('drop:', dragEvent); // for debug
-        console.log('toElement', dragEvent.toElement); // drop先の取得
+        // console.log('drop:', dragEvent); // for debug
+        // console.log('toElement', dragEvent.toElement); // drop先の取得
         this.setPosition(
           this.offsetX + dragEvent.pageX - this.startX,
           this.offsetY + dragEvent.pageY - this.startY);
@@ -77,33 +68,46 @@ export class DragHandleComponent implements OnInit, AfterViewInit, OnDestroy {
       });
 
       this.subscriptions.dragend = dragend$.subscribe((dragendEvent: DragEvent) => {
-        console.log('dragend', dragendEvent); // for debug
+        // console.log('dragend', dragendEvent); // for debug
         overlayPane.style.opacity = '1';
         this.subscriptions.drop.unsubscribe();
         this.subscriptions.dragend.unsubscribe();
         this.subscriptions.dragover.unsubscribe();
       });
-    });
 
-    // for window resize
-    const resize$ = Observable.fromEvent(window, 'resize');
-    this.subscriptions.resize = resize$.subscribe((resizeEvent: UIEvent) => {
-      console.log('resize', resizeEvent);
-      this.setPosition(this.offsetX, this.offsetY);
+      this.subscriptions.dragover = dragover$.subscribe((dragoverEvent: DragEvent) => {
+        dragoverEvent.preventDefault();
+      });
+
+      const instance: DraggableCompoent = componentRef.instance;
+      this.setDragImage(event, instance);
+      this.startX = event.pageX;
+      this.startY = event.pageY;
+  
+      setTimeout(function() {
+        overlayPane.style.opacity = '0';
+      }, 0);  
     });
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.setPosition(this.offsetX, this.offsetY);
   }
 
   ngAfterViewInit() {
     this.setPosition(
-      (document.body.clientWidth - this.overlayRef.overlayElement.clientWidth) / 2,
+      (this.document.body.clientWidth - this.overlayRef.overlayElement.clientWidth) / 2,
       window.innerHeight - this.overlayRef.overlayElement.clientHeight);
   }
 
   ngOnDestroy() {
-    for (let key in this.subscriptions) {
-      let eventSubscription:Subscription = this.subscriptions[key];
-      if (eventSubscription) {
-        eventSubscription.unsubscribe();
+    for (const key in this.subscriptions) {
+      if (this.subscriptions.hasOwnProperty(key)) {
+        const eventSubscription: Subscription = this.subscriptions[key];
+        if (eventSubscription) {
+          eventSubscription.unsubscribe();
+        }
       }
     }
   }
@@ -120,7 +124,7 @@ export class DragHandleComponent implements OnInit, AfterViewInit, OnDestroy {
       this.offsetY = Math.max(0, y);
       if (this.overlayRef.overlayElement) {
         this.offsetX = Math.min(this.offsetX,
-          document.body.clientWidth - this.overlayRef.overlayElement.clientWidth);
+          this.document.body.clientWidth - this.overlayRef.overlayElement.clientWidth);
         this.offsetY = Math.min(this.offsetY,
           window.innerHeight - this.overlayRef.overlayElement.clientHeight);
       }
