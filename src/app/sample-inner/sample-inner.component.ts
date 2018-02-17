548fccfdd350eb1816902bcf7db5f895658843e3
cloudliner <cloudliner.jp@gmail.com>
2018-02-17 20:41:09 +0900

    show size and types

--- src/app/sample-inner/sample-inner.component.ts
+++ src/app/sample-inner/sample-inner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { BreakpointState, BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs/Observable';
 
@@ -8,26 +8,59 @@ import { Observable } from 'rxjs/Observable';
   styleUrls: ['./sample-inner.component.scss']
 })
 export class SampleInnerComponent implements OnInit {
-  isHandset: Observable<BreakpointState>;
+  isHandsetPortrait: boolean;
+  isHandset: boolean;
+  isTabletPortrait: boolean;
+  isTablet: boolean;
+  isWebPortrait: boolean;
+  isWeb: boolean;
+  width: number;
+  height: number;
 
-  constructor(bm: BreakpointObserver) {
-    console.log(bm);
-    bm.observe([
-      Breakpoints.HandsetLandscape,
-      Breakpoints.HandsetPortrait
-    ]).subscribe((state: BreakpointState) => {
-      if (state.matches) {
-        console.log('state matches:', state);
-      } else {
-        console.log('state unmatch:', state);
-      }
-    });
-    this.isHandset = bm.observe(Breakpoints.Handset);
-    this.isHandset.subscribe((state:BreakpointState) => {
-      console.log('state:', state);
-    });
+  @HostListener('window:resize')
+  onResize() {
+    this.showSize();
   }
 
   ngOnInit() {
+    this.showSize();
+  }
+
+  constructor(bo: BreakpointObserver) {
+    bo.observe([
+      Breakpoints.HandsetPortrait
+    ]).subscribe((state: BreakpointState) => {
+      this.isHandsetPortrait = state.matches;
+    });
+    bo.observe([
+      Breakpoints.Handset
+    ]).subscribe((state: BreakpointState) => {
+      this.isHandset = state.matches;
+    });
+    bo.observe([
+      Breakpoints.TabletPortrait
+    ]).subscribe((state: BreakpointState) => {
+      this.isTabletPortrait = state.matches;
+    });
+    bo.observe([
+      Breakpoints.Tablet
+    ]).subscribe((state: BreakpointState) => {
+      this.isTablet = state.matches;
+    });
+    bo.observe([
+      Breakpoints.WebPortrait
+    ]).subscribe((state: BreakpointState) => {
+      this.isWebPortrait = state.matches;
+    });
+    bo.observe([
+      Breakpoints.Web
+    ]).subscribe((state: BreakpointState) => {
+      this.isWeb = state.matches;
+    });
+  }
+
+  private showSize() {
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
   }
 }
