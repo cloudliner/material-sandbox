7d0c4b43e90dd2acddef9ef302e74c3d9c917c92
cloudliner <cloudliner.jp@gmail.com>
2018-02-18 17:06:43 +0900

    add animation

--- src/app/fade-in/fade-in.component.ts
+++ src/app/fade-in/fade-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { trigger, state, transition, style, animate } from '@angular/animations';
 import { CdkPortal } from '@angular/cdk/portal';
 import { Overlay } from '@angular/cdk/overlay';
 import { OverlayConfig } from '@angular/cdk/overlay';
@@ -6,7 +7,25 @@ import { OverlayConfig } from '@angular/cdk/overlay';
 @Component({
   selector: 'app-fade-in',
   templateUrl: './fade-in.component.html',
-  styleUrls: ['./fade-in.component.scss']
+  styleUrls: ['./fade-in.component.scss'],
+  animations: [
+    trigger('flyInOut', [
+      state('in', style({opacity: 1, transform: 'translateX(0)'})),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100%)'
+        }),
+        animate('0.2s ease-in')
+      ]),
+      transition('* => void', [
+        animate('0.2s 0.1s ease-out', style({
+          opacity: 0,
+          transform: 'translateX(-100%)'
+        }))
+      ])
+    ])
+  ]
 })
 export class FadeInComponent implements OnInit {
   @ViewChild(CdkPortal) templatePortal: CdkPortal;
