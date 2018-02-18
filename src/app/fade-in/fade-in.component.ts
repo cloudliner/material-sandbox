a1349bdb4713faeef2bd28e5755b670cbf469dff
cloudliner <cloudliner.jp@gmail.com>
2018-02-18 18:24:37 +0900

    add items

--- src/app/fade-in/fade-in.component.ts
+++ src/app/fade-in/fade-in.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { trigger, state, transition, style, animate } from '@angular/animations';
+import { trigger, state, transition, style, animate, query, stagger } from '@angular/animations';
 import { CdkPortal } from '@angular/cdk/portal';
 import { Overlay } from '@angular/cdk/overlay';
 import { OverlayConfig } from '@angular/cdk/overlay';
+
+interface PoiItem {
+  name: string;
+}
 
 @Component({
   selector: 'app-fade-in',
@@ -24,12 +28,33 @@ import { OverlayConfig } from '@angular/cdk/overlay';
           transform: 'translateX(-100%)'
         }))
       ])
+    ]),
+    trigger('staggerItems', [
+      transition('0 => *', [
+        query(':enter', [
+          style({
+            transform: 'translateX(-100%)'
+          }),
+          stagger(200, animate(500, style({
+            transform: 'translateX(0)'
+          })))
+        ])
+      ])
+    ]),
+    trigger('newItem', [
+      transition(':enter', [
+        style({
+          transform: 'translateY(-50%)'
+        }),
+        animate('0.2s ease-in')
+      ])
     ])
   ]
 })
 export class FadeInComponent implements OnInit {
   @ViewChild(CdkPortal) templatePortal: CdkPortal;
   @ViewChild('fadeInParent') fadeInParent: ElementRef;
+  items: PoiItem[] = [];
   private overlayConfig: OverlayConfig;
 
   constructor(private overlay: Overlay) {
@@ -47,14 +72,32 @@ export class FadeInComponent implements OnInit {
       positionStrategy,
     });
     const overlayRef = this.overlay.create(this.overlayConfig);
-    this.templatePortal.attach(overlayRef);
+    // this.templatePortal.attach(overlayRef);
   }
 
-  click(event) {
-    console.log('click', event);
+  show(event) {
+    console.log('show', event);
     if (!this.templatePortal.isAttached) {
       const overlayRef = this.overlay.create(this.overlayConfig);
       this.templatePortal.attach(overlayRef);
+      this.items = [
+        { name: 'John Titor' },
+        { name: 'Mori' },
+        { name: 'Alice' }
+      ];
+    }
+  }
+  hide(event) {
+    console.log('hide', event);
+    if (this.templatePortal.isAttached) {
+      this.templatePortal.detach();
+      this.items = [];
+    }
+  }
+  add(event) {
+    console.log('add', event);
+    if (this.templatePortal.isAttached) {
+      this.items.push({ name: 'Yuta' });
     }
   }
 }
